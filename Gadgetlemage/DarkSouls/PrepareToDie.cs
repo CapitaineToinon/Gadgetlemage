0bbd3240f10a4de35f920a8a8680b4e848b99d49
CapitaineToinon <antoinesutter@gmail.com>
2021-04-24 00:42:26 +0200

    Fixed PTDE to work with pirated version

--- Gadgetlemage/DarkSouls/PrepareToDie.cs
+++ Gadgetlemage/DarkSouls/PrepareToDie.cs
@@ -13,14 +13,18 @@ namespace Gadgetlemage.DarkSouls
         private const string INVENTORY_DATA_AOB = "A1 ? ? ? ? 53 55 8B 6C 24 10 56 8B 70 08 32 DB 85 F6";
         private const string FLAGS_AOB = "56 8B F1 8B 46 1C 50 A1 ? ? ? ? 32 C9";
         private const string CHR_DATA_AOB = "83 EC 14 A1 ? ? ? ? 8B 48 04 8B 40 08 53 55 56 57 89 4C 24 1C 89 44 24 20 3B C8";
+        private const string FUNC_ITEM_GET_AOB = "55 8B EC 83 E4 F8 83 EC 34 8B 4D 0C 53 8B 5D 08 56 83 C8 FF 33 F6 81 F9 00 00 00 20 57 89 44 24 1C 89 74 24 20 89 B3 8C 01 00 00 89 44 24 18";
+        private const uint FUNC_ITEM_CALL_OFFSET = 0x26; // the offset of the instrcution in the asm that will call the item function
         private const uint INVENTORY_INDEX_START = 0x1B8;
-        private const uint FUNC_ITEM_GET_PTR = 0xC0B6DA;
 
         /// <summary>
         /// Properties
         /// </summary>
         public PHPointer pBasePtr { get; private set; }
         public PHPointer pInventoryData { get; private set; }
+        public PHPointer pFuncItemGet { get; private set; }
+
+        
 
         /// <summary>
         /// Constructor
@@ -35,6 +39,7 @@ namespace Gadgetlemage.DarkSouls
 
             pBasePtr = Process.RegisterAbsoluteAOB(BASE_PTR_AOB, 2);
             pInventoryData = Process.RegisterAbsoluteAOB(INVENTORY_DATA_AOB, 1);
+            pFuncItemGet = Process.RegisterAbsoluteAOB(FUNC_ITEM_GET_AOB);
 
             Process.RescanAOB();
         }
@@ -49,15 +54,16 @@ namespace Gadgetlemage.DarkSouls
                 byte[] asm = (byte[])Assembly.PTDE.Clone();
 
                 // Get the pointer to CharBasePtr
-                IntPtr pointer = pBasePtr.Resolve();
-                pointer = Process.CreateChildPointer(pBasePtr, 0, 8).Resolve();
+                pBasePtr.Resolve();
+                IntPtr pointer = Process.CreateChildPointer(pBasePtr, 0, 8).Resolve();
 
                 // Have to allocate first to rebase the code
                 IntPtr memory = Process.Allocate((uint)asm.Length);
-                uint funcPointer = (uint)(FUNC_ITEM_GET_PTR - (uint)memory);
+                uint funcPointer = (uint)pFuncItemGet.Resolve() - (uint)memory - FUNC_ITEM_CALL_OFFSET;
 
                 // Now we can write the rebased bytes
-                byte[] bytes = BitConverter.GetBytes((ulong)pointer + INVENTORY_INDEX_START);
+                byte[] bytes;
+                bytes = BitConverter.GetBytes((ulong)pointer + INVENTORY_INDEX_START);
                 Array.Copy(bytes, 0, asm, 0x1, 4);
                 bytes = BitConverter.GetBytes(ItemCategory);
                 Array.Copy(bytes, 0, asm, 0x6, 4);
@@ -65,12 +71,12 @@ namespace Gadgetlemage.DarkSouls
                 Array.Copy(bytes, 0, asm, 0xB, 4);
                 bytes = BitConverter.GetBytes(ItemQuantity);
                 Array.Copy(bytes, 0, asm, 0x10, 4);
-                bytes = BitConverter.GetBytes((ulong)funcPointer);
+                bytes = BitConverter.GetBytes(funcPointer);
                 Array.Copy(bytes, 0, asm, 0x22, 4);
 
                 // Write, Execute and Free
                 Kernel32.WriteBytes(Process.Handle, memory, asm);
-                int result = Process.Execute(memory);
+                Process.Execute(memory);
                 Process.Free(memory);
             }
         }
@@ -104,12 +110,11 @@ namespace Gadgetlemage.DarkSouls
         /// <param name="weapon"></param>
         public override void DeleteItem(BlackKnightWeapon weapon)
         {
-            InventoryItem[] result = new InventoryItem[0];
-
             if (Process.Hooked)
             {
-                result = new InventoryItem[2048];
-                IntPtr pointer = pInventoryData.Resolve();
+                InventoryItem[] result = new InventoryItem[2048];
+                
+                pInventoryData.Resolve();
                 PHPointer pInventory = Process.CreateChildPointer(pInventoryData, 0, 8, 0x2DC);
                 byte[] bytes = pInventory.ReadBytes(0, 2048 * 0x1C);
 
